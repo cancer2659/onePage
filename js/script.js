393ea7de26cf2b9aa7ca4e2d04b2fb19c7418764
cancer2659 <cancermakh@gmail.com>
2020-06-29 08:39:49 +0300

    add window "forgot password"

---
+++ js/script.js
@@ -0,0 +1,41 @@
+$(document).ready(function(){
+
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:0,
+        nav:true,
+        navText: [ '', ' ' ],
+
+        responsive:{
+            0:{
+                items:1
+            },
+
+            1000:{
+                items:1
+            }
+        }
+    });
+});
+
+$( function() {
+    $( "#accordion" ).accordion();
+} );
+
+$( function() {
+    $( "#dialog" ).dialog({
+        autoOpen: false,
+        show: {
+            effect: "blind",
+            duration: 1000
+        },
+        hide: {
+            effect: "explode",
+            duration: 1000
+        }
+    });
+
+    $( "#opener" ).on( "click", function() {
+        $( "#dialog" ).dialog( "open" );
+    });
+} );
